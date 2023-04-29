2730c4a1417c6c02502e939efdc13a87d6bd6c2a
Wanding Zhou <zhouwanding@gmail.com>
2023-04-29 10:36:42 -0400

    minor, message omitted

--- R/askme.R
+++ R/askme.R
@@ -1,12 +1,41 @@
-#' Master data frame for all object to cache
+#' Master data frame for all model objects
 #'
 #' This is an internal object which will be updated on every new release
-#' library(ExperimentHub)
-#' eh <- query(ExperimentHub(localHub=FALSE), c("sesameData", "v1.13.1"))
-#' data.frame(name=eh$title, eh=names(eh))
-#'
-#' @name df_master
+#' 
+#' @name askme_models
 #' @docType data
-#' @return master sheet of sesameData objects
+#' @return master sheet of askme model objects
+#' @export
 NULL
 
+#' Model for CNS cancer type classification
+#'
+#' @name m_cancertype_CNS65
+#' @docType data
+#' @return random forest model
+NULL
+
+#' Model for TCGA cancer type classification
+#'
+#' @name m_cancertype_TCGA33
+#' @docType data
+#' @return random forest model
+NULL
+
+#' classify cancer type
+#'
+#' @param betas DNA methylation betas
+#' @param model cancer classification model
+#' @return predicted cancer type label
+#' @examples
+#' library(sesameData)
+#' betas <- sesameDataGet("HM450.1.TCGA.PAAD")$betas
+#' askme_cancertype(betas, model=m_cancertype_TCGA33)
+#' ## expect PAAD
+#' @import randomForest
+#' @export
+askme_cancertype <- function(betas, model=m_cancertype_TCGA33) {
+    require("randomForest")
+    betas <- betas[match(rownames(model$importance), names(betas))]
+    predict(model, newdata=betas)
+}
