9df4a89a0f66c739fb8f103e9fead26b421a148d
Jacob Fanale <jakefanale@gmail.com>
2024-03-19 13:27:48 -0400

    Review 1 Changes

--- tests/testthat/test_data.R
+++ tests/testthat/test_data.R
@@ -1,25 +1,25 @@
 context("data")
 
-test_that("test cmi_classify returns tibble for RFC", {
+test_that("test cmi_predict returns tibble for RFC", {
   library(tibble)
   library(sesameData)
   library(ExperimentHub)
   betas <- sesameDataGet("HM450.1.TCGA.PAAD")$betas
   eh <- ExperimentHub()
   modelrfc <- eh[["EH8395"]]
-  result <- CytoMethIC::cmi_classify(betas, modelrfc, lift_over=TRUE)
+  result <- CytoMethIC::cmi_predict(betas, modelrfc, lift_over=TRUE)
   expect_is(result, "tbl_df")
 })
 
 
-test_that("test cmi_classify returns tibble for SVM", {
+test_that("test cmi_predict returns tibble for SVM", {
   library(tibble)
   library(sesameData)
   library(ExperimentHub)
   betas <- sesameDataGet("HM450.1.TCGA.PAAD")$betas
   eh <- ExperimentHub()
   modelrfc <- eh[["EH8396"]]
-  result <- CytoMethIC::cmi_classify(betas, modelrfc, lift_over=TRUE)
+  result <- CytoMethIC::cmi_predict(betas, modelrfc, lift_over=TRUE)
   expect_is(result, "tbl_df")
 })
 
